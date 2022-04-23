fb8e601d4967421e36cd3096ce37f60179dba257
Mike Hermsen <MikeHermsenPrive@Gmail.com>
2022-04-23 20:38:08 +0200

    Added new command

--- ISongPage.cs
+++ ISongPage.cs
@@ -13,7 +13,9 @@ namespace spotify {
             Console.WriteLine("Use goto {page.name} to navigate");
                 Console.WriteLine();
             Console.WriteLine("goto song.panel");
-            Console.WriteLine("song.current     - view current song"); 
+            Console.WriteLine("song.current                                     - view current song"); 
+            Console.WriteLine("song.make {title} {duration in sec} {genre}      - maakt song aan"); 
+            
         }
         
 
